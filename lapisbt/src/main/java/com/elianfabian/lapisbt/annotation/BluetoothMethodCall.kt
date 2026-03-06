ff77456af42b8a9898e2e866dbe10913b64db86f
Elián Fabián <elian.fabian.2001@gmail.com>
2026-03-06 16:30:21 +0100

    Remove BluetoothMethodCall and BluetoothParam annotations

--- lapisbt/src/main/java/com/elianfabian/lapisbt/annotation/BluetoothMethodCall.kt
+++
@@ -1,7 +0,0 @@
-package com.elianfabian.lapisbt.annotation
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-public annotation class BluetoothMethodCall(
-	val name: String,
-)
--- lapisbt/src/main/java/com/elianfabian/lapisbt/annotation/BluetoothParam.kt
+++
@@ -1,7 +0,0 @@
-package com.elianfabian.lapisbt.annotation
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-public annotation class BluetoothParam(
-	val name: String,
-)
