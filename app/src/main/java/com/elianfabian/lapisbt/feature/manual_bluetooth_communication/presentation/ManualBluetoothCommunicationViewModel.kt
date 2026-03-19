b9ec6fa900b064fb97438d229d175af0f5d77adb
Elián Fabián <elian.fabian.2001@gmail.com>
2026-03-19 16:04:56 +0100

    Rename LapistBt event

--- app/src/main/java/com/elianfabian/lapisbt/feature/manual_bluetooth_communication/presentation/ManualBluetoothCommunicationViewModel.kt
+++ app/src/main/java/com/elianfabian/lapisbt/feature/manual_bluetooth_communication/presentation/ManualBluetoothCommunicationViewModel.kt
@@ -222,7 +222,7 @@ class ManualBluetoothCommunicationViewModel(
 					is LapisBt.Event.OnPairingFailed -> {
 						// no-op
 					}
-					is LapisBt.Event.OnUnexpectedPairedDevice -> {
+					is LapisBt.Event.OnUnexpectedDevicePaired -> {
 						// no-op
 					}
 				}
