e30dede5fa7d8a0966663deeadd18c51a357bf0f
Elián Fabián <elian.fabian.2001@gmail.com>
2026-03-19 12:35:33 +0100

    Add more device classes and better organize the code

--- lapisbt/src/main/java/com/elianfabian/lapisbt/model/BluetoothDevice.kt
+++ lapisbt/src/main/java/com/elianfabian/lapisbt/model/BluetoothDevice.kt
@@ -84,6 +84,33 @@ public data class BluetoothDevice(
 			public data class UnknownValue(val value: Int) : AudioVideo
 		}
 
+		public sealed interface Peripheral : DeviceClass {
+			public data object NonKeyboardNonPointing : Peripheral
+			public data object Keyboard : Peripheral
+			public data object Pointing : Peripheral
+			public data object KeyboardPointing : Peripheral
+
+			public data object Joystick : Peripheral
+			public data object Gamepad : Peripheral
+			public data object RemoteControl : Peripheral
+			public data object SensingDevice : Peripheral
+			public data object DigitizerTablet : Peripheral
+			public data object CardReader : Peripheral
+			public data object DigitalPen : Peripheral
+			public data object HandheldScanner : Peripheral
+			public data object HandheldGesturalInput : Peripheral
+
+			public data class UnknownValue(val value: Int) : Peripheral
+		}
+
+		public sealed interface Imaging : DeviceClass {
+			public data object Display : Imaging
+			public data object Camera : Imaging
+			public data object Scanner : Imaging
+			public data object Printer : Imaging
+			public data class UnknownValue(val value: Int) : Imaging
+		}
+
 		public sealed interface Wearable : DeviceClass {
 			public data object Uncategorized : Wearable
 			public data object WristWatch : Wearable
@@ -91,6 +118,7 @@ public data class BluetoothDevice(
 			public data object Jacket : Wearable
 			public data object Helmet : Wearable
 			public data object Glasses : Wearable
+			public data object Pin : Wearable
 			public data class UnknownValue(val value: Int) : Wearable
 		}
 
@@ -105,28 +133,30 @@ public data class BluetoothDevice(
 		}
 
 		public sealed interface Health : DeviceClass {
-			public data object Uncategorized : DeviceClass
-			public data object BloodPressure : DeviceClass
-			public data object Thermometer : DeviceClass
-			public data object Weighing : DeviceClass
-			public data object Glucose : DeviceClass
-			public data object PulseOximeter : DeviceClass
-			public data object PulseRate : DeviceClass
-			public data object DataDisplay : DeviceClass
-			public data class UnknownValue(val value: Int) : Health
-		}
+			public data object Uncategorized : Health
+			public data object BloodPressureMonitor : Health
+			public data object Thermometer : Health
+			public data object WeighingScale : Health
+			public data object GlucoseMeter : Health
+			public data object PulseOximeter : Health
+			public data object HeartPulseRateMonitor : Health
+			public data object DataDisplay : Health
 
-		public sealed interface Peripheral : DeviceClass {
-			public data object NonKeyboardNonPointing : Peripheral
-			public data object Keyboard : Peripheral
-			public data object Pointing : Peripheral
-			public data object KeyboardPointing : Peripheral
-			public data class UnknownValue(val value: Int) : Peripheral
+			public data object StepCounter : Health
+			public data object BodyCompositionAnalyzer : Health
+			public data object PeakFlowMonitor : Health
+			public data object MedicationMonitor : Health
+			public data object KneeProsthesis : Health
+			public data object AnkleProsthesis : Health
+			public data object GenericHealthManager : Health
+			public data object PersonaMobilityDevice : Health
+
+			public data class UnknownValue(val value: Int) : Health
 		}
 	}
 
 	public sealed interface MajorDeviceClass {
-		public data object Misc : MajorDeviceClass
+		public data object Miscellaneous : MajorDeviceClass
 		public data object Computer : MajorDeviceClass
 		public data object Phone : MajorDeviceClass
 		public data object Networking : MajorDeviceClass
@@ -146,5 +176,35 @@ public data class BluetoothDevice(
 		public data object Dual : Type
 		public data object Unknown : Type
 		public data class UnknownValue(val value: Int) : Type
+	}
+
+
+	// Device classes taken from the Bluetooth specification: https://www.bluetooth.com/wp-content/uploads/Files/Specification/Assigned_Numbers.pdf
+	internal object ExtraDeviceClass {
+		const val PERIPHERAL_JOYSTICK = 0x501
+		const val PERIPHERAL_GAMEPAD = 0x502
+		const val PERIPHERAL_REMOTE_CONTROL = 0x503
+		const val PERIPHERAL_SENSING_DEVICE = 0x504
+		const val PERIPHERAL_DIGITIZER_TABLET = 0x505
+		const val PERIPHERAL_CARD_READER = 0x506
+		const val PERIPHERAL_DIGITAL_PEN = 0x507
+		const val PERIPHERAL_HANDHELD_SCANNER = 0x508
+		const val PERIPHERAL_HANDHELD_GESTURAL_INPUT = 0x509
+
+		const val IMAGING_DISPLAY = 0x601
+		const val IMAGING_CAMERA = 0x602
+		const val IMAGING_SCANNER = 0x604
+		const val IMAGING_PRINTER = 0x608
+
+		const val WEARABLE_PIN = 0x718
+
+		const val HEALTH_STEP_COUNTER = 0x0920
+		const val HEALTH_BODY_COMPOSITION_ANALYZER = 0x0924
+		const val HEALTH_PEAK_FLOW_MONITOR = 0x0928
+		const val HEALTH_MEDICATION_MONITOR = 0x092C
+		const val HEALTH_KNEE_PROSTHESIS = 0x0930
+		const val HEALTH_ANKLE_PROSTHESIS = 0x0934
+		const val HEALTH_GENERIC_HEALTH_MANAGER = 0x0938
+		const val HEALTH_PERSONAL_MOBILITY_DEVICE = 0x093C
 	}
 }
