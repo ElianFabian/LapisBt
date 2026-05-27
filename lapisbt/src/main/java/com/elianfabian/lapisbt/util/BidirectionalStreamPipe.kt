2b86e97e644f4eef29dd9b9d69e1c2d993049c07
Elián Fabián <elian.fabian.2001@gmail.com>
2026-05-27 20:51:59 +0200

    Fix BidirectionalStreamPipe implementation
    After sometime when reading the input stream it throwed this exception: "java.io.IOException: Pipe broken"
    
    For more information: https://stackoverflow.com/q/1866255/18418162

--- lapisbt/src/main/java/com/elianfabian/lapisbt/util/BidirectionalStreamPipe.kt
+++ lapisbt/src/main/java/com/elianfabian/lapisbt/util/BidirectionalStreamPipe.kt
@@ -4,17 +4,23 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
+import java.nio.channels.Channels
+import java.nio.channels.Pipe
 
 /**
  * Creates two bidirectional pipes cross-linked to represent two sides of a connection.
  * Data written to [sideA.outputStream] is readable from [sideB.inputStream], and vice versa.
  */
-public class BidirectionalStreamPipe(bufferSize: Int = 65536) {
-    private val outA = PipedOutputStream()
-    private val inB = PipedInputStream(outA, bufferSize)
-    
-    private val outB = PipedOutputStream()
-    private val inA = PipedInputStream(outB, bufferSize)
+public class º(bufferSize: Int = 65536) {
+
+    private val pipeA = Pipe.open()
+    private val pipeB = Pipe.open()
+
+    private val outA = Channels.newOutputStream(pipeA.sink())
+    private val inB = Channels.newInputStream(pipeA.source())
+
+    private val outB = Channels.newOutputStream(pipeB.sink())
+    private val inA = Channels.newInputStream(pipeB.source())
 
     public val sideA: ConnectionSide = ConnectionSide(inA, outA)
     public val sideB: ConnectionSide = ConnectionSide(inB, outB)
