023d63d06978ff1a2259ef0fee91b9319931c1f1
Elián Fabián <elian.fabian.2001@gmail.com>
2026-06-23 16:35:34 +0200

    Prepare to publish library version 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.LibraryExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
 	repositories {
@@ -13,4 +15,38 @@ plugins {
 	alias(libs.plugins.kotlinAndroid) apply false
 	alias(libs.plugins.kotlinParcelize) apply false
 	//alias(libs.plugins.kotlinSerialization) apply false
+	id("maven-publish")
+}
+
+subprojects {
+	val libraryModules = listOf("lapisbt", "lapisbt-rpc", "lapisbt-logger")
+	if (name in libraryModules) {
+		val libs = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
+		group = libs.findVersion("lapisbtGroup").get().requiredVersion
+		version = libs.findVersion("lapisbtVersion").get().requiredVersion
+
+		plugins.withId("com.android.library") {
+			plugins.apply("maven-publish")
+			configure<LibraryExtension> {
+				publishing {
+					singleVariant("release") {
+						withSourcesJar()
+					}
+				}
+			}
+			afterEvaluate {
+				extensions.configure<PublishingExtension> {
+					publications {
+						create<MavenPublication>("release") {
+							from(components.findByName("release") ?: components["release"])
+
+							groupId = project.group.toString()
+							artifactId = project.name
+							version = project.version.toString()
+						}
+					}
+				}
+			}
+		}
+	}
 }
